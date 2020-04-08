0379afadab50727ab517cef1bd057214297497aa
johnmorris4 <johnmorris4@bellsouth.net>
2020-04-08 04:22:42 -0400

    Adjusted NameSpace

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace ConsoleApp
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello John");
+            Console.ReadLine();
         }
     }
 }
